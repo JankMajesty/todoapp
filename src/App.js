b25e24b23b1113bfb7571344e9274d91e8a42a49
JankMajesty <lmcgill36@gmail.com>
2021-09-13 19:08:14 -0500

    pushing changes and delete button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoCard from './TodoCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lucas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isClicked: true,
+      inputValue: "",
+      listOfTodos: []
+    }
+  }
+
+  handleClick = () => {
+    this.state.isClicked ? 
+      this.setState({isClicked : false}) : 
+        this.setState({isClicked: true})
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+      event.preventDefault()
+      this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]})
+      this.setState({inputValue: ""})
+  }
+
+  deleteItem = (index) => {
+    console.log("was clicked", index)
+    let copyOfList = this.state.listOfTodos
+    copyOfList.splice(index, 1)
+    this.setState({listOfTodos: [...copyOfList]})
+  }
+
+  componentDidMount() {
+    console.log("runs on mount")
+    console.log(this.state.isClicked);
+  }
+
+  componentDidUpdate() {
+    console.log("runs on update")
+    console.log(this.state.isClicked);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}> 
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+          <button type="submit" onClick={(this.handleClick)}>Submit here</button>
+        </form>
+        <ol>{this.state.listOfTodos.map((todo, index) => {
+          return <TodoCard key={index} index={index} title={todo} clickToRemove={this.deleteItem} />
+        })}</ol>
+      </div>
+      );
+    }
+  }
 
 export default App;
